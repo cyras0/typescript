3a03f5c4bd42b895c0687836945e5944564839ef
Gary Xia <ilovegators@gmail.com>
2025-05-28 09:02:44 -0700

    work as JS Mastery, form cool

--- yc_directory/app/components/Navbar.tsx
+++ yc_directory/app/components/Navbar.tsx
@@ -1,51 +1,51 @@
-'use client'
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signOut, signIn } from "@/auth";
 
-import Link from 'next/link'
-import Image from 'next/image'
-import React from 'react'
-import {signOut, signIn} from 'next-auth/react'
-import {useSession} from 'next-auth/react'
+const Navbar = async () => {
+  const session = await auth();
 
-const Navbar = () => {
-    const {data: session} = useSession()
- 
-    return (
-        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-            <nav className="flex justify-between items-center">
-                <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={144} height={30} />
-                </Link>
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
 
-                <div className="flex items-center gap-5 text-black">
-                    {session?.user ? (
-                        <>
-                            <Link href="/startup/create">
-                                <span>Create Startup</span>         
-                            </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create Startup</span>         
+              </Link>
 
-                            <button 
-                                onClick={() => signOut()}
-                                className="hover:text-gray-600"
-                            >
-                                Sign Out
-                            </button>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Sign Out</button>
+              </form>
 
-                            <Link href={`/user/${session.user.id}`}>
-                                <span>{session.user.name}</span>
-                            </Link>
-                        </>
-                    ) : (
-                        <button 
-                            onClick={() => signIn('github')}
-                            className="hover:text-gray-600"
-                        >
-                            Login with GitHub
-                        </button>
-                    )}
-                </div>
-            </nav>
-        </header>
-    )
-}
+              <Link href={`/user/${session.user.id}`}>
+                <span>{session.user.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
