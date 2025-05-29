8b52a8bb0a6c84dc1b58dae31b91ca0047e0a8ef
Gary Xia <ilovegators@gmail.com>
2025-05-28 21:49:00 -0700

    Search btn added, use client

--- yc_directory/app/(root)/page.tsx
+++ yc_directory/app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import SearchForm from "@/components/SearchForm";
+
 export default function Home() {
   return (
     <>    
@@ -18,7 +20,10 @@ export default function Home() {
           <h2 className="text-3xl font-bold text-center mb-12">All Startups</h2>
           {/* Startup listings will go here */}
         </div>
+
+        <SearchForm />
       </section>
+
     </>
   );
 }
--- yc_directory/app/components/SearchForm.tsx
+++ yc_directory/app/components/SearchForm.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Form from 'next/form'
+import SearchFormReset from './SearchFormReset';
 
 const SearchForm = () => {
-  return (
-    <div>SearchForm</div>
-  )
+    const query = 'Test';
+
+    const reset = () => {
+        const form = document.querySelector('.search-form') as HTMLFormElement;
+        if (form) form.reset();
+    };
+
+    return (
+        <Form action="/" scroll={false} className="search-form">
+            <input
+                name="query"
+                defaultValue={query}
+                placeholder="Search Startups"
+                className="search-input px-4 py-2 border rounded-md"
+            />
+
+            <div className="flex gap-2">
+                {query && <SearchFormReset />}
+
+                <button type="submit" className="search-btn text-white">
+                    Search
+                </button>
+            </div>
+        </Form>
+    )
 }
 
 export default SearchForm
