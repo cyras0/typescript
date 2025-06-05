9585cb501546cff6b4732f85554772d7b0093545
Gary Xia <ilovegators@gmail.com>
2025-06-04 20:33:52 -0700

    ui done

--- loom/app/(auth)/sign-in/page.tsx
+++ loom/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline font-karla">Auth</h1>
-    </div>
+    <main className="sign-in">
+      <aside className='testimonial'>
+          <div className='description'>
+            <section>
+              <figure>
+                {Array.from({ length: 5}).map((_, index) => (
+                  <Image src="/assets/icons/star.svg" alt="star" width={20} height={20} key={index}/>
+                ))}
+              </figure>
+              <p>SnapCast makes screen recording easy. From quick walkthroughs to full tutorials, it is fast, smooth, and sharable in seconds</p>
+              <article>
+                <Image src="/assets/images/jason.png" alt="jason" width={64} height={64} className='rounded-full' />
+                <div>
+                  <h2>Jason Rivera</h2>
+                  <p>Product Designer, Google</p>
+                </div>
+              </article>
+            </section>
+        </div>
+        <p>© SnapCast {(new Date()).getFullYear()}. All rights reserved.</p>
+      </aside>
+      <aside className='google-sign-in'>
+        <section>
+         <Link href="/">
+          <Image src="/assets/icons/logo.svg" alt="logo" width={40} height={40} />
+          <h1>SnapCast</h1>
+         </Link>
+         <p>Create and share your very first <span>SnapCast video</span> in no time!</p>
+         <button>
+          <Image src="/assets/icons/google.svg" alt="google" width={22} height={22} />
+          <span>Sign in with Google</span>
+         </button>
+        </section>
+      </aside>
+      <div className='overlay'/>
+    </main>
   )
 }
 
